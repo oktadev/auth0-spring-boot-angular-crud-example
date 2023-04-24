03b0b3938de198ecb250642836a34262671aed4e
Matt Raible <matt@raibledesigns.com>
2023-04-23 20:47:18 -0600

    Fix Maven tests

--- src/test/java/com/okta/developer/jugtours/JugtoursApplicationTests.java
+++ src/test/java/com/okta/developer/jugtours/JugtoursApplicationTests.java
@@ -3,7 +3,7 @@ package com.okta.developer.jugtours;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {JugtoursApplication.class, TestSecurityConfiguration.class})
 class JugtoursApplicationTests {
 
 	@Test
