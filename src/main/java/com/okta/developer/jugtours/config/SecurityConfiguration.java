8c02c43756c1489e28d32e496774fb179b6db647
Matt Raible <matt@raibledesigns.com>
2023-04-26 13:16:51 -0600

    Fix deprecation warnings

--- src/main/java/com/okta/developer/jugtours/config/SecurityConfiguration.java
+++ src/main/java/com/okta/developer/jugtours/config/SecurityConfiguration.java
@@ -10,25 +10,23 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
-            .requestMatchers("/", "/index.html", "*.ico", "*.css", "*.js", "/api/user").permitAll()
-            .anyRequest().authenticated()
-        );
-
-        http.oauth2Login();
-        http.oauth2ResourceServer().jwt();
-
-        http.csrf()
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler());
-        http.addFilterAfter(new CookieCsrfFilter(), BasicAuthenticationFilter.class);
-
-        http.addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class);
+                .requestMatchers("/", "/index.html", "*.ico", "*.css", "*.js", "/api/user").permitAll()
+                .anyRequest().authenticated())
+            .oauth2Login(withDefaults())
+            .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()))
+            .csrf((csrf) -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+            .addFilterAfter(new CookieCsrfFilter(), BasicAuthenticationFilter.class)
+            .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class);
 
         return http.build();
     }
