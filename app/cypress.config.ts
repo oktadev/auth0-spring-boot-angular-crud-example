0c270aa4b010555b5119f6b3d55e55aea33197e4
Matt Raible <matt@raibledesigns.com>
2023-04-28 17:34:57 -0600

    Change timeout from 4 to 10 seconds

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-
   e2e: {
     'baseUrl': 'http://localhost:4200'
   },
-
   video: false,
-
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
-    },
-    specPattern: '**/*.cy.ts'
-  }
-
+    }
+  },
+  defaultCommandTimeout: 10000,
 })
