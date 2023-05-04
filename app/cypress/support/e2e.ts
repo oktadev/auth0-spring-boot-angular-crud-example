e1bd1dcf2c84a5c900ef899620907d82fc5c47f6
Matt Raible <matt@raibledesigns.com>
2023-05-03 22:51:33 -0600

    Add better error handling

--- app/cypress/support/e2e.ts
+++ app/cypress/support/e2e.ts
@@ -1,12 +1,13 @@
 import './commands';
 
 beforeEach(() => {
-  cy.visit('/')
-  cy.get('#login').click()
   if (Cypress.env('E2E_USERNAME') === undefined) {
-    console.error('E2E_USERNAME is not defined, skipping login');
+    console.error('E2E_USERNAME is not defined');
+    alert('E2E_USERNAME is not defined');
     return;
   }
+  cy.visit('/')
+  cy.get('#login').click()
   cy.login(
     Cypress.env('E2E_USERNAME'),
     Cypress.env('E2E_PASSWORD')
